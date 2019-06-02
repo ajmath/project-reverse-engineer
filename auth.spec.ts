9b99038eebd152ce44c112e26afd7f19a4311ea2
Andrew Matheny <andrew.j.matheny@gmail.com>
2019-06-02 15:14:32 -0400

    Creates basic test framework

---
+++ auth.spec.ts
@@ -0,0 +1,57 @@
+import { Request } from "express";
+import { authenticate } from "./auth";
+
+function mockRequest(authorizationHeader?: string): Request {
+  return {
+    headers: {
+      authorization: authorizationHeader,
+    }
+  } as any as Request;
+}
+
+describe("auth.ts", () => {
+
+  beforeEach(() => {
+    process.env.AUDIENCE = "test-aud";
+  });
+
+  it("should revoke request without valid authentication method", async () => {
+    const req = mockRequest("Bad auth method");
+    const result = await authenticate(req);
+    expect(result).toEqual({
+      authenticated: false, 
+      message: "Unsupported Authentication Method", 
+      status: 403
+    });
+  });
+
+  it("should revoke request without auth header", async () => {
+    const req = mockRequest(undefined);
+    const result = await authenticate(req);
+    expect(result).toEqual({
+      authenticated: false, 
+      message: "Authentication header is empty", 
+      status: 403
+    });
+  });
+
+  it("should revoke request with invalid basic auth", async () => {
+    const req = mockRequest("Basic InvalidBasicAuth");
+    const result = await authenticate(req);
+    expect(result).toEqual({
+      authenticated: false, 
+      message: "Basic Auth Error", 
+      status: 403
+    });
+  });
+
+  it("should revoke request with invalid MSAuth1.0 auth", async () => {
+    const req = mockRequest("MSAuth1.0 InvalidAuth");
+    const result = await authenticate(req);
+    expect(result).toEqual({
+      authenticated: false, 
+      message: "S2S Auth Error: Invalid Bearer Token - missing field", 
+      status: 403
+    });
+  });
+});
